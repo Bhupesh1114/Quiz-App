d96dba98b827f745d2ae4cd0543925076eb0de87
Bhupesh kumawat <Bhupeshkumawat100@gmail.com>
2021-08-27 00:56:24 +0530

    Quiz

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import questions from "./components/Questions";
+import "./App.css";
 
 function App() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+
+  const [showScore, setShowScore] = useState(false);
+
+  const [score, setScore] = useState(0);
+
+  const handleAnswerButtonClick = (isCorrect) => {
+    if (isCorrect === true) {
+      alert("Your answer is correct");
+      setScore(score +1)
+    } else {
+      alert("Your answer is wrong")
+}
+
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      {showScore ? (
+        <div
+          className="d-flex justify-content-center align-items-center "
+          style={{ height: "100vh" }}
         >
-          Learn React
-        </a>
-      </header>
+          <div className="bg-info h3 text-white p-4" style={{ borderRadius: "5px" }}>
+            You scored {score} out of {questions.length}
+          </div>
+        </div>
+      ) : (
+        <div
+          className="d-flex justify-content-center align-items-center "
+          style={{ height: "100vh" }}
+        >
+            <div>
+              
+            <h2 className="text-center">RCC Quiz</h2>
+            <div
+              className="d-flex bg-info text-white p-4"
+              style={{ borderRadius: "5px" }}
+            >
+              <div>
+                  <h4 className="text-dark">Question {currentQuestion +1}/{questions.length}</h4>
+                <h4 className="mr-3" style={{ maxWidth: "300px" }}>
+                  {questions[currentQuestion].questionText}
+                </h4>
+              </div>
+              <div>
+                {questions[currentQuestion].answerOptions.map(
+                  (answerOption) => (
+                    <button
+                      onClick={() => handleAnswerButtonClick(answerOption.isCorrect)}
+                      className="btn-sm btn-dark mb-1 d-block"
+                    >
+                      {answerOption.answerText}
+                    </button>
+                  )
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/components/Questions.js
@@ -0,0 +1,58 @@
+const questions = [
+  {
+        questionText: "In a combined footing if shear stress exceeds 5 kg/cm2, the nominal stirrups provided are:",
+        answerOptions: [
+            { answerText: "6 legged", isCorrect: false },
+            { answerText: "8 legged", isCorrect: false },
+            { answerText: "10 legged", isCorrect: false },
+            { answerText: "12 legged", isCorrect: true }
+      ]
+  },
+    {
+        questionText: "Thickened part of a flat slab over its supporting column, is technically known as",
+        answerOptions: [
+            { answerText: "drop panel", isCorrect: true},
+            { answerText: "capital ", isCorrect: false },
+            { answerText: "column head", isCorrect: false },
+            { answerText: "none of these.", isCorrect: false }
+        ]
+    },
+  {
+      questionText: "The number of treads in a flight is equal to",
+      answerOptions: [
+        { answerText: "risers in the flight", isCorrect: false },
+        { answerText: "risers plus one", isCorrect: false },
+        { answerText: "risers minus one", isCorrect: true },
+        { answerText: "none of these.", isCorrect: false }
+    ]
+  },
+  {
+      questionText: "If the permissible compressive stress for a concrete in bending is C kg/m2, the modular ratio is",
+      answerOptions: [
+        { answerText: "2800/C ", isCorrect: false },
+        { answerText: "	2300/2C", isCorrect: false },
+        { answerText: "2800/3C", isCorrect: true },
+        { answerText: "2800/C2", isCorrect: false }
+    ]
+  },
+  {
+      questionText: "If T and R are tread and rise respectively of a stair, then",
+      answerOptions: [
+        { answerText: "	2R + T = 60", isCorrect: true },
+        { answerText: "R + 2T = 60", isCorrect: false},
+        { answerText: "	2R + T = 30", isCorrect: false },
+        { answerText: "R + 2T= 30", isCorrect: false }
+    ]
+  },
+    {
+        questionText: "For stairs spanning horizontally, the minimum waist provided is",
+        answerOptions: [
+            { answerText: "4 cm", isCorrect: false },
+            { answerText: "7 cm", isCorrect: false },
+            { answerText: "9 cm", isCorrect: false },
+            { answerText: "12 cm", isCorrect: true }
+        ]
+  },
+];
+
+export default questions;
